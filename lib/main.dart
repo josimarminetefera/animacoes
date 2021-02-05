c92d1989b2fbd040262a4e2ae771a5019dc6c962
josimarminetefera <josimaminete@gmail.com>
2021-02-05 19:08:53 -0300

    Adicionado execuçãosem o setSate()

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,14 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     //inicializar o animation
     //tween animação vai variar o nimero de 0 a 300 enquando o controller vai variar de 0 a 1
     //tween é uma animaçao com inicio e fim
-    animacao = Tween<double>(begin: 0, end: 300).animate(controller);
-
-    //animação tem que dar o start e atializar a tela
-    animacao.addListener(() {
-      //chama esta função sempre que tiver uma modificaçao na animação
-      setState(() {});
-    });
+    animacao = Tween<double>(begin: 0, end: 300).animate(controller);;
+      //animação tem que dar o start e atializar a tela
+      //estes .. é um parametros de cascata ele espera o primeiro terminar e depois faz o proximo
+      //..addListener(() {
+        //foi removido o setstate pois foi crido a classAnimacaoLogo
+        //chama esta função sempre que tiver uma modificaçao na animação
+        //setState(() {});
+      //});
 
     //iniciar a animação
     controller.forward();
@@ -59,10 +60,25 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    return AnimacaoLogo(animacao);
+  }
+}
+
+//isso aqui serva para remover o setState
+class AnimacaoLogo extends AnimatedWidget {
+  //a animação que chegar vai ser enviada para o super
+  //este super é o contrutor do AnimatedWidget
+  AnimacaoLogo(Animation<double> animacao) : super(listenable: animacao);
+
+  @override
+  Widget build(BuildContext context) {
+    //listenable precisa de um tipo
+    final Animation<double> animacao_local = listenable;
+
     return Center(
       child: Container(
-        height: animacao.value,
-        width: animacao.value,
+        height: animacao_local.value,
+        width: animacao_local.value,
         child: FlutterLogo(),
       ),
     );
